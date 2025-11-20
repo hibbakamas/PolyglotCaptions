021888d651b4e8e5b3c1969886456b45bff5eba6
Gaby <gvega.ieu2021@student.ie.edu>
2025-11-20 14:58:13 +0100

    database schema

---
+++ app/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.Captions (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    CreatedAt DATETIME2 NOT NULL,
+    SessionId NVARCHAR(100) NULL,
+    FromLang NVARCHAR(10) NOT NULL,
+    ToLang NVARCHAR(10) NOT NULL,
+    Transcript NVARCHAR(MAX) NOT NULL,
+    TranslatedText NVARCHAR(MAX) NOT NULL,
+    ProcessingMs INT NOT NULL
+);
